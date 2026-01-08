1d92569c3dadf1c963e149b05e42e7594e478bae
CCH2655 <chengch2655@gmail.com>
2026-01-09 00:35:24 +0800

    [update] README; 註解刪除操作; 樣式調整;

--- src/App.jsx
+++ src/App.jsx
@@ -104,137 +104,148 @@ function App() {
   useEffect(() => {
     // 頁面載入直接執行一次"檢查登入狀態"
     // main.jsx StrictMode 會執行第二次
-    checkLogin();
+    // checkLogin();
   }, []);
 
   return (
     <>
-      
+      <button
+        className={`fixed m-2 btn ${!isAuth? "btn-danger" : "btn-success"}`}
+        type="button"
+        onClick={() => checkLogin()}
+      >
+        {!isAuth? "確認是否登入" : "已登入"}
+      </button>
 
       {!isAuth ? (
-        <div className="container login">
-          <div className="row justify-content-center">
-            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div className="row">
-              <div className="col-2">
-                <button
-                  className="btn btn-danger"
-                  type="button"
-                  onClick={() => checkLogin()}
-                >
-                  確認是否登入
-                </button>
+        <div className="container ">
+          <div className="position-absolute top-50 start-50 translate-middle card">
+            <div className="card-body">
+              <div className="row justify-content-center">
+                <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
+
+                <div className="col-8">
+                  <form id="form" className="form-signin" 
+                  onSubmit={handleSubmit}
+                  >
+                    <div className="form-floating mb-3">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="username"
+                        name="username"
+                        placeholder="name@example.com"
+                        value={formData.username}
+                        onChange={handleInputChange}
+                        required
+                        autoFocus
+                      />
+                      <label htmlFor="username">Email address</label>
+                    </div>
+                    <div className="form-floating">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        required
+                      />
+                      <label htmlFor="password">Password</label>
+                    </div>
+                    <button
+                      className="btn btn-lg btn-primary w-100 mt-3"
+                      type="submit"
+                    >
+                      登入
+                    </button>
+                  </form>
+                </div>
               </div>
-            </div>
-            <div className="col-6">
-              <form id="form" className="form-signin" 
-              onSubmit={handleSubmit}
-              >
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="username"
-                    name="username"
-                    placeholder="name@example.com"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                    required
-                    autoFocus
-                  />
-                  <label htmlFor="username">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <button
-                  className="btn btn-lg btn-primary w-100 mt-3"
-                  type="submit"
-                >
-                  登入
-                </button>
-              </form>
+              <p className="mt-5 mb-3 text-muted">&copy; 2025~∞ - 六角學院</p>
             </div>
           </div>
-          <p className="mt-5 mb-3 text-muted">&copy; 2025~∞ - 六角學院</p>
+          
         </div>
       ) : (
         // 登入後的產品管理頁面 (同第一週)
         <div className="container">
           <div className="row mt-5">
             <div className="col-md-6">
-              <h2>產品列表</h2>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>產品名稱</th>
-                    <th>原價</th>
-                    <th>售價</th>
-                    <th>是否啟用</th>
-                    <th>查看細節</th>
-                    <th>更多操作</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {products.map((item, index) => (
-                    <tr key={index}>
-                      <td>{item.title}</td>
-                      <td>{item.origin_price}</td>
-                      <td>{item.price}</td>
-                      <td>
-                        {item.is_enabled ? "啟用" : "未啟用"}
-                      </td>
-                      <td>
-                        <button type="button" className="btn btn-primary" onClick={() => setTempProduct(item)}>查看細節</button>
-                      </td>
-                      <td>
-                        <button type="button" className="btn btn-danger" onClick={() => deleteData(item.id)}>刪除</button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+              <div className="card">
+                <div className="card p-4">
+                  <h2>產品列表</h2>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>產品名稱</th>
+                        <th>原價</th>
+                        <th>售價</th>
+                        <th>是否啟用</th>
+                        <th>查看細節</th>
+                        {/* <th>更多操作</th> */}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {products.map((item, index) => (
+                        <tr key={index}>
+                          <td>{item.title}</td>
+                          <td>{item.origin_price}</td>
+                          <td>{item.price}</td>
+                          <td>
+                            {item.is_enabled ? "啟用" : "未啟用"}
+                          </td>
+                          <td>
+                            <button type="button" className="btn btn-primary" onClick={() => setTempProduct(item)}>查看細節</button>
+                          </td>
+                          {/* <td>
+                            <button type="button" className="btn btn-danger" onClick={() => deleteData(item.id)}>刪除</button>
+                          </td> */}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              
             </div>
 
             <div className="col-md-6 ">
-              <h2>單一產品細節</h2>
-              {tempProduct ? (
-                <div className="card mb-3 p-2">
-                  <img src={tempProduct.imageUrl} className="card-img-top primary-image" alt="主圖" />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      {tempProduct.title}
-                      <span className="badge bg-primary ms-2">{tempProduct.category}</span>
-                    </h5>
-                    <p className="card-text">商品描述：{tempProduct.description}</p>
-                    <p className="card-text">商品內容：{tempProduct.content}</p>
-                    <div className="d-flex">
-                      <p className="card-text text-secondary"><del>{tempProduct.origin_price}</del></p>
-                      元 / {tempProduct.price} 元
+              <div className="card">
+                <div className="card p-4">
+                  <h2>單一產品細節</h2>
+                  {tempProduct ? (
+                    <div className="card mb-3 p-2">
+                      <img src={tempProduct.imageUrl} className="card-img-top primary-image" alt="主圖" />
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          {tempProduct.title}
+                          <span className="badge bg-primary ms-2">{tempProduct.category}</span>
+                        </h5>
+                        <p className="card-text">商品描述：{tempProduct.description}</p>
+                        <p className="card-text">商品內容：{tempProduct.content}</p>
+                        <div className="d-flex">
+                          <p className="card-text text-secondary"><del>{tempProduct.origin_price}</del></p>
+                          元 / {tempProduct.price} 元
+                        </div>
+                        <h5 className="mt-3">更多圖片：</h5>
+                        <div className="d-flex flex-wrap">
+                          {tempProduct.imagesUrl.map((url, index) => {
+                            return (
+                              <img key={index} src={url} className="images" />
+                            )
+                          })}
+                        </div>
+                      </div>
                     </div>
-                    <h5 className="mt-3">更多圖片：</h5>
-                    <div className="d-flex flex-wrap">
-                      {tempProduct.imagesUrl.map((url, index) => {
-                        return (
-                          <img key={index} src={url} className="images" />
-                        )
-                      })}
-                    </div>
-                  </div>
+                  ) : (
+                    <p className="text-secondary">請選擇一個商品查看</p>
+                  )}
                 </div>
-              ) : (
-                <p className="text-secondary">請選擇一個商品查看</p>
-              )}
+              </div>
+              
             </div>
           </div>
         </div>
